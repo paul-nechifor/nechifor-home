ff5b724b530049c9201e6d47214848bbebea9834
Paul Nechifor <paul@nechifor.net>
2017-06-19 09:50:59 -0400

    Use ESLint with Airbnb style.

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,23 @@
-var fs = require('fs');
-var gulp = require('gulp');
-var htmlmin = require('gulp-htmlmin');
-var pug = require('gulp-pug');
-var webserver = require('gulp-webserver');
+const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const pug = require('gulp-pug');
+const webserver = require('gulp-webserver');
 
 gulp.task('default', ['html', 'webserver', 'watch']);
 
-gulp.task('html', function () {
-  return gulp.src('index.pug')
-  .pipe(pug({}))
-  .pipe(htmlmin({collapseWhitespace: true}))
-  .pipe(gulp.dest('dist'));
+gulp.task('html', () => {
+  gulp.src('index.pug')
+    .pipe(pug({}))
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('webserver', function () {
-  var port = parseInt(process.env.port || '8080', 10);
+gulp.task('webserver', () => {
+  const port = parseInt(process.env.port || '8080', 10);
   gulp.src('dist')
-  .pipe(webserver({livereload: true, open: true, port: port, host: '0.0.0.0'}));
+    .pipe(webserver({ livereload: true, open: true, port, host: '0.0.0.0' }));
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', () => {
   gulp.watch('index.pug', ['html']);
 });
